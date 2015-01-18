d2287a6d0e4b0af7b8e075747f0f71eba3037f67
Henrik Bohlin <olleorm89@gmail.com>
2015-01-18 16:35:04 +0100

    (First commit) A program that makes a 2Darray and assign a random number (1-10) in every tile and write the total sum off each row.

---
+++ src/tenxten.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+/**
+ * Created by Olleorm on 2015-01-17.
+ */
+public class tenxten {
+
+    private static int randomInt(int from, int to) {
+        Random rand = new Random();
+        return rand.nextInt(to - from + 1) + from;
+    }
+
+    int oneToTen = randomInt(1, 10);
+
+
+    public static void main(String[] args) {
+
+        int [][] twoDim = new int [10][10];
+
+        int a = (twoDim.length);
+        int b = (twoDim[0].length);
+
+        for(int i = 0; i < a; i++){
+            int sum = 0;
+            for(int j = 0; j < b; j++) {
+                int x = (randomInt(1, 10));
+                twoDim[i][j] = x;
+                sum += x;
+
+                if (x < 10) {
+                    System.out.print(" " + x + " ");
+                } else {
+                    System.out.print(x + " ");
+                }
+                if (j == 9){
+                    System.out.print(sum);
+                }
+            }//end of for J
+            System.out.println();
+        }//end of for i
+
+
+    }
+
+
+}
