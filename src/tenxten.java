8f4af785873b54e04301156f886ed1fd4918037d
Henrik Bohlin <olleorm89@gmail.com>
2015-01-18 23:45:46 +0100

    IT WORKS

--- src/tenxten.java
+++ src/tenxten.java
@@ -4,20 +4,39 @@ import java.util.Random;
  * Created by Olleorm on 2015-01-17.
  */
 public class tenxten {
+    static int numberRows = 10;
+    static int numberColums = 10;
+    static int [][] twoDim = new int [numberColums][numberRows];
+    static int a = (twoDim.length);
+    static int b = (twoDim[0].length);
+
 
     private static int randomInt(int from, int to) {
         Random rand = new Random();
         return rand.nextInt(to - from + 1) + from;
     }
 
-    int oneToTen = randomInt(1, 10);
+    private static void numbersNr() {
+        int[] numbers = new int[numberColums * numberRows];
+        int conter = 0;
+
+        for (int i = 1; i < 10; i++) {
+            for (int y = 0; y < 10; y++) {
+                for (int x = 0; x < 10; x++) {
+                    if ((twoDim[y][x] / (double) i) == 1) {
+                        numbers[i] += i;
+
+                    }
+                }
+            }
+            System.out.println(" " + numbers[i] / i + " " + i + "s" );
+        }
+    }
+
 
 
     public static void main(String[] args) {
-        int numberRows = 10;
-        int numberColums = 10;
 
-        int [][] twoDim = new int [numberColums][numberRows];
 
         int a = (twoDim.length);
         int b = (twoDim[0].length);
@@ -41,7 +60,7 @@ public class tenxten {
             System.out.println();
         }//end of for i
 
-
+        numbersNr();
     }
 
 
