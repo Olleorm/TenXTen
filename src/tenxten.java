38ab3012a417b080f658ced06bc3edbe4b4e9073
Henrik Bohlin <olleorm89@gmail.com>
2015-01-19 02:06:33 +0100

    added sumOfColumns

--- src/tenxten.java
+++ src/tenxten.java
@@ -6,21 +6,19 @@ import java.util.Random;
  */
 public class tenxten {
     static int numberRows = 10;
-    static int numberColums = 10;
-    static int [][] twoDim = new int [numberColums][numberRows];
+    static int numberColumns = 10;
+    static int [][] grid = new int [numberColumns][numberRows];
 
     private static int randomInt(int from, int to) {
         Random rand = new Random();
         return rand.nextInt(to - from + 1) + from;
     }
     private static void amountOfSpecificNumbers() {
-        int[] numbers = new int[numberColums * numberRows];
-        int conter = 0;
-
+        int[] numbers = new int[numberColumns * numberRows];
         for (int i = 1; i < 10; i++) {
             for (int y = 0; y < 10; y++) {
                 for (int x = 0; x < 10; x++) {
-                    if ((twoDim[y][x] / (double) i) == 1) {
+                    if ((grid[y][x] / (double) i) == 1) {
                         numbers[i] += i;
                     }
                 }
@@ -29,24 +27,35 @@ public class tenxten {
         }
     }
 
-    private static void sumOfRows() {
-        int sumOfRows[] = new int[numberColums];
-        for (int x = 0; x < numberColums; x++) {
+    private static void sumOfColumns() {
+        int sumOfColumns[] = new int[numberColumns];
+        for (int x = 0; x < numberColumns; x++) {
             for (int y = 0; y < numberRows; y++) {
-                sumOfRows[x] += twoDim[x][y];
+                sumOfColumns[y] += grid[x][y];
+            }
+        }
+        System.out.println(Arrays.toString(sumOfColumns));
+    }
+
+
+    private static void sumOfRows() {
+        int sumOfRows[] = new int[numberColumns];
+        for (int x = 0; x < numberColumns; x++) {
+            for (int y = 0; y < numberRows; y++) {
+                sumOfRows[x] += grid[x][y];
             }
         }
         System.out.println(Arrays.toString(sumOfRows));
     }
 
     private static void newFeild() {
-        int row = (twoDim.length);
-        int colum = (twoDim[0].length);
+        int row = (grid.length);
+        int colum = (grid[0].length);
         for (int x = 0; x < row; x++) {
             int sum = 0;
             for (int y = 0; y < colum; y++) {
                 int randomNumber = (randomInt(1, 10));
-                twoDim[x][y] = randomNumber;
+                grid[x][y] = randomNumber;
                 sum += randomNumber;
                 if (randomNumber < 10) {
                     System.out.print(" " + randomNumber + " ");
@@ -64,7 +73,7 @@ public class tenxten {
         newFeild();
 
         sumOfRows();
-
+        sumOfColumns();
         amountOfSpecificNumbers();
 
     }
